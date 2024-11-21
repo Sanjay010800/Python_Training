020bd0c8275cbf85c967e21befecf3d44b5a7569
Sanjay010800 <sanjaysadan00@gmail.com>
2024-11-21 19:18:13 +0530

    mongo demo

---
+++ MongoDB_ Trails/mongodemo.py
@@ -0,0 +1,5 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import  ServerApi
+
+uri= r"mongodb+srv://rohithrajreganti:rohithrajreganti@cluster0.rd7dn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
